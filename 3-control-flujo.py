338730e0957bbda8d3a937a617e68b8b94eb6111
kmacedo0409 <kmacedo0409@gmail.com>
2022-10-20 15:08:38 -0500

    agregando archivos del dia jueves13/10

---
+++ 3-control-flujo.py
@@ -0,0 +1,12 @@
+# IF - ELSE
+
+# input() > sirve para ingresar valores por la terminal, el input siempre registrara un str, 
+# TODO: convertir ese valor de str a int
+edad = input()
+
+if (edad >= 18) :
+    print('Eres mayor de edad')
+    print('felicidades')
+else:
+    print('No eres mayor de edad, no puedes ingresar')
+print('termino el programa')
