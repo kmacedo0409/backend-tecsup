e4a142a96ae7a0c9f662f8254c04757e652467e6
kmacedo0409 <kmacedo0409@gmail.com>
2022-11-09 11:06:41 -0500

    creacion de un endpoint

--- dia-3/app.py
+++ dia-3/app.py
@@ -9,7 +9,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Karinita1989@localhost:330
 conexion.init_app(app)
 #indicaremos la creacion de las nuevas tablas que no se encuentran registradas en la base de datos PERO esos modelos, 
 # tiene que ser utilizado en el proyecto para que se pueda crear la tabla en la base de datos, emitira un error si no logro conectar a la base de datos
-# 
+# PARA UTILIZAR EL METODO CREATE_ALL NECESITA ESTA DENTRODE UN ENDPOINT O UNA FUNCIONABILIDAD DE FLASK, EN ESTE CASOE STAMOS UTILIZANDO UN METODO LLAMDO BEFORE_FIRST
+# QUE SE JECUTARA ANTES DE REALIZAR LA PRIMERA PETICION DE LA API.
 
 def inicializacion():
     conexion.create_all()
@@ -17,8 +18,13 @@ app.before_first_request(inicializacion)
 
 @app.route('/participantes', methods=['GET'])
 def participantes():
+    # SELECT * FROM participantes
     resultado = conexion.session.query(ParticipanteModel).all()
     print(resultado)
+    print(resultado[0].nombre)
+    print(resultado[0].apellido)
+    return{
+        'message': 'Los participantes son'
+    }
     
-
 app.run(debug=True)
