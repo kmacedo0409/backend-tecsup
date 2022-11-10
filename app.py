0f5c8ead5a367d86a0056dc72bafaa4d9733eb1c
kmacedo0409 <kmacedo0409@gmail.com>
2022-11-09 23:05:02 -0500

    finalizacion migraciones flask

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 from flask import Flask
+from config import conexion
+from flask_migrate import Migrate
+from os import environ
+from dotenv import load_dotenv
+from models.usuarios import UsuarioModel
 
-app=Flask(__name__)
 
-app.run(debug=True)
+# para cargar las variables del archivo .env para que puedan ser utilizadas como variables de entorno
+load_dotenv()
+
+app =Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
+# inicializamos la instancia de flask-SQLAlchemy con las propiedades seteadas en la aplicación de flask
+conexion.init_app(app)
+
+# Inicializamos la clase migrate con la configuracion de nuestra base de datos y aplicacion de flask
+migrate = Migrate(app, conexion)
+
+if __name__ == '__main__':
+    app.run(debug=True)
