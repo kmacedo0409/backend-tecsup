5bb95b38a5334e3a677d94e3e7aa61fe1da17974
kmacedo0409 <kmacedo0409@gmail.com>
2022-11-30 21:59:44 -0500

    agregue allowed host

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-6me7s_k$qytudl1=(3kb$x+fz7vg5#+9xgqsmtj(sqpu6@3$!y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['restaurante-django-karina.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
